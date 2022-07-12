4441b43bf49ee2acaf1ff5152891e0899a781b46
Kieran Jeffrey-Smart <kjeffreysmar@vmware.com>
2022-07-12 11:51:53 +0100

    Add modal feature for images

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -23,7 +23,78 @@ class BlogPostTemplate extends React.Component {
     return (
       <Layout location={location}>
         <style>
-          {`
+          {`          
+          /* The Modal (background) */
+          .modal {
+            display: none; /* Hidden by default */
+            position: fixed; /* Stay in place */
+            z-index: 1; /* Sit on top */
+            padding-top: 100px; /* Location of the box */
+            left: 0;
+            top: 0;
+            width: 100%; /* Full width */
+            height: 100%; /* Full height */
+            overflow: auto; /* Enable scroll if needed */
+            background-color: rgb(0,0,0); /* Fallback color */
+            background-color: rgba(0,0,0,0.9); /* Black w/ opacity */
+          }
+
+          /* Modal Content (image) */
+          .modal-content {
+            margin: auto;
+            display: block;
+            width: 80%;
+            max-width: 700px;
+          }
+
+          /* Caption of Modal Image */
+          #caption {
+            margin: auto;
+            display: block;
+            width: 80%;
+            max-width: 700px;
+            text-align: center;
+            color: #ccc;
+            padding: 10px 0;
+            height: 150px;
+          }
+
+          /* Add Animation */
+          .modal-content, #caption {  
+            -webkit-animation-name: zoom;
+            -webkit-animation-duration: 0.6s;
+            animation-name: zoom;
+            animation-duration: 0.6s;
+          }
+
+          @-webkit-keyframes zoom {
+            from {-webkit-transform:scale(0)} 
+            to {-webkit-transform:scale(1)}
+          }
+
+          @keyframes zoom {
+            from {transform:scale(0)} 
+            to {transform:scale(1)}
+          }
+
+          /* The Close Button */
+          .close {
+            position: absolute;
+            top: 15px;
+            right: 35px;
+            color: #f1f1f1;
+            font-size: 40px;
+            font-weight: bold;
+            transition: 0.3s;
+          }
+
+          .close:hover,
+          .close:focus {
+            color: #bbb;
+            text-decoration: none;
+            cursor: pointer;
+          }
+
           .post-content {
             text-align: justify;
           }
@@ -37,6 +108,9 @@ class BlogPostTemplate extends React.Component {
               width: calc(100% + ${rhythm((3 / 4) * 2)});
               margin-left: ${rhythm(-3 / 4)};
               height: ${rhythm(13)};
+            }
+            .modal-content {
+              width: 100%;
             }
           }
         `}
@@ -79,6 +153,36 @@ class BlogPostTemplate extends React.Component {
           className="post-content"
           dangerouslySetInnerHTML={{ __html: post.content }}
         />
+        
+        <div id="myModal" class="modal">
+          <span id="modal-close" class="close">&times;</span>
+          <img class="modal-content" id="img01" />
+          <div id="caption"></div>
+        </div>
+        <script>{`
+          // Get the modal
+          var modal = document.getElementById("myModal");
+
+          // Get the image and insert it inside the modal - use its "alt" text as a caption
+          var images = document.getElementsByClassName("modal-image");
+          images.forEach((img, i) => {
+            var modalImg = document.getElementById("img01");
+            var captionText = document.getElementById("caption");
+            img.onclick = function(){
+              modal.style.display = "block";
+              modalImg.src = this.src;
+              captionText.innerHTML = this.alt;
+            }
+          }
+
+          // Get the <span> element that closes the modal
+          var span = document.getElementById("modal-close");
+
+          // When the user clicks on <span> (x), close the modal
+          span.onclick = function() { 
+            modal.style.display = "none";
+          }`}
+        </script>
         <hr
           style={{
             marginBottom: rhythm(1),
